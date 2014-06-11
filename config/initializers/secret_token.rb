05718091c603f999ac5c3ea9a1578b8775d65421
charlyy <cyoon90@yahoo.com>
2014-06-11 12:25:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Showerthoughts::Application.config.secret_key_base = '6272b716f134a8d72531ef081ecdeb666bc565121c392ab9cc3f869bfd6ffd3b8a6a499d33a6c900445bff642715e06a6a019266a0aeaa787a272472c8cfe561'
